12ebf8674d2c5e7beed58089ca6065951def5493
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-08-18 09:14:36 -0600

    gulp-nodemon
    Added gulp-nodemon to the gulpfile, so that gulp and nodemon play nice.
    Dev environment looking pretty good.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var gulp        = require('gulp'),
     babel       = require('gulp-babel'),
     data        = require('gulp-data'),
     stylus      = require('gulp-stylus'),
-    htmlmin  = require('gulp-htmlmin');
+    htmlmin     = require('gulp-htmlmin'),
+    nodemon     = require('gulp-nodemon'),
+    path       = require('path');
 
 const paths = {
   scripts:  ['src/public/**/*.js'],
@@ -17,9 +19,34 @@ const paths = {
     src:    ['src/server/**/*.js'],
     build:  'build/server'
   }
-}
+};
+
+const extensions = 'html js styl';
 
 gulp.task('default', ['scripts', 'html', 'stylus', 'server']);
+
+gulp.task('dev', function () {
+  nodemon({
+    script: './build/server/server.js'
+    , env: {'NODE_ENV': 'development'}
+    , ext: extensions
+    , tasks: (changedFiles) => {
+      return changedFiles.filter((file) => {
+        const tasks = [];
+        console.log('file ==', file, '\ndirname ==', path.dirname(file));
+        if (path.extname(file) === '.js') {
+          if (path.dirname(file).includes('src/server') && !~tasks.indexOf('server')) {
+            tasks.push('server');
+          } else if (!~tasks.indexOf('scripts')) {
+            tasks.push('scripts')
+          }
+        }
+        if (path.extname(file) === '.styl' && !~tasks.indexOf('stylus')) tasks.push('stylus');
+        if (path.extname(file) === '.html' && !~tasks.indexOf('html')) tasks.push('html');
+      });
+    }
+  })
+});
 
 gulp.task('scripts', () => {
   return gulp.src(paths.scripts)
