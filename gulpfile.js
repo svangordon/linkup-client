f3de5b70c628bbb5e664d497558033197688550b
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2016-08-18 10:31:14 -0600

    gulpfile dev task dependencies
    added a bunch of dependencies to the dev task, so it should now properly
    clean /dist before writing. Seems like it works properly

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,41 @@ var gulp        = require('gulp'),
     stylus      = require('gulp-stylus'),
     htmlmin     = require('gulp-htmlmin'),
     nodemon     = require('gulp-nodemon'),
-    path       = require('path');
+    path        = require('path'),
+    del         = require('del'),
+    Cache       = require('gulp-file-cache');
+
+const cache = new Cache();
 
 const paths = {
   scripts:  ['src/public/**/*.js'],
   stylus:   ['src/public/**/*.styl'],
   html:     ['src/public/**/*.html'],
-  public:   'build/public',
+  public:   'dist/public',
   server: {
     src:    ['src/server/**/*.js'],
-    build:  'build/server'
+    dist:  'dist/server'
   }
 };
 
 const extensions = 'html js styl';
 
-gulp.task('default', ['scripts', 'html', 'stylus', 'server']);
+gulp.task('default', ['dev', 'scripts', 'html', 'stylus', 'server', 'clean']);
 
-gulp.task('dev', function () {
-  nodemon({
-    script: './build/server/server.js'
+gulp.task('clean', () => {
+  return del(['dist']);
+});
+
+gulp.task('dev', ['scripts', 'html', 'stylus', 'server', 'clean'], () => {
+  return stream = nodemon({
+    script: './dist/server/server.js'
     , env: {'NODE_ENV': 'development'}
     , ext: extensions
+    , watch: 'src'
     , tasks: (changedFiles) => {
       return changedFiles.filter((file) => {
         const tasks = [];
-        console.log('file ==', file, '\ndirname ==', path.dirname(file));
+        // console.log('file ==', file, '\ndirname ==', path.dirname(file));
         if (path.extname(file) === '.js') {
           if (path.dirname(file).includes('src/server') && !~tasks.indexOf('server')) {
             tasks.push('server');
@@ -45,11 +54,12 @@ gulp.task('dev', function () {
         if (path.extname(file) === '.html' && !~tasks.indexOf('html')) tasks.push('html');
       });
     }
-  })
+  });
+  return stream;
 });
 
-gulp.task('scripts', () => {
-  return gulp.src(paths.scripts)
+gulp.task('scripts', ['clean'], () => {
+  const stream = gulp.src(paths.scripts)
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
@@ -57,9 +67,10 @@ gulp.task('scripts', () => {
     .pipe(concat('bundle.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.public));
+  return stream;
 });
 
-gulp.task('html', () => {
+gulp.task('html', ['clean'], () => {
   return gulp.src(paths.html)
     .pipe(sourcemaps.init())
     .pipe(htmlmin({
@@ -69,7 +80,7 @@ gulp.task('html', () => {
     .pipe(gulp.dest(paths.public));
 });
 
-gulp.task('stylus', () => {
+gulp.task('stylus', ['clean'], () => {
   return gulp.src(paths.stylus)
     .pipe(sourcemaps.init())
     .pipe(stylus())
@@ -77,12 +88,13 @@ gulp.task('stylus', () => {
     .pipe(gulp.dest(paths.public));
 });
 
-gulp.task('server', () => {
-  return gulp.src(paths.server.src)
+gulp.task('server', ['clean'], () => {
+  const stream = gulp.src(paths.server.src)
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.server.build))
-  });
+    .pipe(gulp.dest(paths.server.dist))
+  return stream;
+});
